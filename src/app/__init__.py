9a45eed836e42dccef9877924482dda90fa4869d
Gabriel <gabicarbonell@gmail.com>
2019-03-08 17:52:34 +0100

    New URI's

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -24,7 +24,7 @@ class ConfigClass(object):
 
 
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root:password@localhost/scimatch?charset=utf8'    # File-based SQL database'    # File-based SQL database
-
+    # SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://scimatch:password@mmb.pcb.ub.es/scimatch?charset=utf8'
 
     SQLALCHEMY_COMMIT_ON_TEARDOWN = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False    # Avoids SQLAlchemy warning
